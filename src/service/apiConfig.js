1ffacdea79b45dc0e9a2c9d75562d463b76081ae
Saksham Pardesi <sakshampardesi5831@gmail.com>
2024-09-30 14:39:04 +0530

    url changed

--- src/service/apiConfig.js
+++ src/service/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://bill-system-eight.vercel.app/',
     timeout: 30000,
     headers: {
       'Content-Type': 'application/json'
