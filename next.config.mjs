7cd3a0683bf5b158de1608f0fe9f6bbf5c7a7834
Saksham Pardesi <sakshampardesi5831@gmail.com>
2024-09-30 14:26:30 +0530

    fix issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import withPWA from 'next-pwa';
 const nextConfig = {};
 
-export default withPWA({
-    dest: 'public',
-  })(nextConfig);
+// export default withPWA({
+//     dest: 'public',
+//   })(nextConfig);
+export default nextConfig;
